6b8a301df7e068e5598d56cf9c6f161032dfff44
Julian Grinblat <julian@dotcore.co.il>
2016-03-29 21:48:27 +0900

    Fix linting

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,16 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const eslint = require('gulp-eslint');
 
+const src = 'lib/index.js';
+
 gulp.task('lint', () =>
-  gulp.src('index.js')
+  gulp.src(src)
   .pipe(eslint())
   .pipe(eslint.format())
 );
 
 gulp.task('build', ['lint'], () => (
-  gulp.src('lib/index.js')
+  gulp.src(src)
   .pipe(babel())
   .pipe(gulp.dest('dist'))
 ));
