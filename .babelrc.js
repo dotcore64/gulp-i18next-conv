b4af716c5ece46992b561f02e3c0ab787bdab236
Julian Grinblat <julian@dotcore.co.il>
2017-11-21 21:27:21 +0900

    Reformat babelrc.js

--- .babelrc.js
+++ .babelrc.js
@@ -1,8 +1,10 @@
 const env = process.env.NODE_ENV || 'production';
+const targets = env === 'test' ? { node: 'current' } : { node: '4' };
 
 module.exports = {
-  presets: [['env', { targets: { node: 4 }, debug: true }]],
-  plugins: ['transform-object-rest-spread'].concat(
-    env === 'test' ? ['istanbul'] : [],
-  ),
+  presets: [['env', { targets }]],
+  plugins: [
+    'transform-object-rest-spread',
+    ...env === 'test' ? ['istanbul'] : [],
+  ],
 }
