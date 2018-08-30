1d42dff53ce34e8f5881e7acf4b01f5dde5e7324
Julian Grinblat <julian@dotcore.co.il>
2018-08-30 13:46:27 +0900

    Upgrade to babel 7

--- .babelrc.js
+++ .babelrc.js
@@ -1,10 +1,9 @@
 const env = process.env.NODE_ENV || 'production';
-const targets = env === 'test' ? { node: 'current' } : { node: '4' };
+const targets = env === 'test' ? { node: 'current' } : { node: '6' };
 
 module.exports = {
   presets: [['@babel/env', { targets }]],
   plugins: [
-    '@babel/proposal-object-rest-spread',
     ...env === 'test' ? ['istanbul', '@babel/transform-block-scoping'] : [],
   ],
 }
