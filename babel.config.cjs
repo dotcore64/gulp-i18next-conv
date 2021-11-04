ebad10732c0a39c0a180946386c44bc54a23d5e8
Julian Grinblat <julian@dotcore.co.il>
2021-11-04 14:26:47 +0900

    Fix linting

--- babel.config.cjs
+++ babel.config.cjs
@@ -2,5 +2,5 @@ module.exports = ({ env }) => ({
   plugins: env('test')
     ? ['rewire']
     : ['@babel/proposal-throw-expressions'],
-  only: env('test') ? ["dist/*.js"] : ["src/*.js"]
+  only: env('test') ? ['dist/*.js'] : ['src/*.js'],
 });
